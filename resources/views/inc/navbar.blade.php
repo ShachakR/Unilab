5898d37f577768b304f25c82175deb4949ede044
ShachakR <59149573+ShachakR@users.noreply.github.com>
2021-11-24 20:08:23 -0500

    ui update

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,47 +1,50 @@
-<nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-dark h5">
+<nav class="navbar fixed-top navbar-expand-sm navbar-light bg-light h4">
     <div class="container-fluid">
-        @if (Auth::guest())
-        <a class="navbar-brand" href="{{ url('/') }}">UNILAB</a>
-        @else
-        <a class="navbar-brand" href="{{ url('/home') }}">UNILAB</a>
-        @endif
+        <div>
+            @if (Auth::guest())
+            <a class="navbar-brand" href="{{ url('/') }}">UNILAB</a>
+            @else
+            <a class="navbar-brand" href="{{ url('/home') }}">UNILAB</a>
+            @endif
+        </div>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-            <div class="navbar-nav">
-                <ul class="navbar-nav">
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mx-auto align-items-stretch">
                     @if (Auth::guest())
                     @else
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ url('/home') }}">Home</a>
+                        <a class="nav-link" aria-current="page" href="{{ url('/home') }}"> <i class="material-icons-outlined nv-icon">home</i> <span> Home </span> </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="">Profile</a>
+                        <a class="nav-link" aria-current="page" href=""> <i class="material-icons-outlined nv-icon">perm_identity</i> <span> Profile </span></a>
                     </li>
                     @endif
                 </ul>
-            </div>
-            <div class="navbar-nav">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav ml-auto ">
                     @if (Auth::guest())
-                    <li class="nav-item ">
+                    <li class="nav-item">
                         <a class="nav-link " href="{{ route('login') }}">Log in</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('register') }}">Register</a>
                     </li>
                     @else
-                    <li class="dropdown nav-item navbar-dark bg-dark">
+                    <li class="nav-item">
                         <a class="nav-link" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        <i class="material-icons-outlined nv-icon">expand_more</i>
                             {{ Auth::user()->username }} <span class="caret"></span>
                         </a>
-                        <ul class="dropdown-menu navbar-dark bg-dark" role="menu">
-                            <li class="nav-item"><a class="nav-link" href="">Settings</a></li>
-                            <li>
-                                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        <ul class="dropdown-menu dropdown-menu-right" role="menu">
+                            <li class="nav-item">
+                                <a class="nav-link drop-menu" href=""><i class="material-icons-outlined nv-icon">settings</i>Settings</a></li>
+                            <li class="nav-item">
+                                <a class="nav-link drop-menu" href="{{ route('logout') }}" onclick="event.preventDefault();
                                               document.getElementById('logout-form').submit();">
+                                <i class="material-icons-outlined nv-icon">logout</i>
                                     Logout
                                 </a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -50,9 +53,9 @@
                             </li>
                         </ul>
                     </li>
+                    @endif
                 </ul>
-                @endif
             </div>
-        </div>
+
     </div>
 </nav>
