3b668bbbf6b0c3543ae6ad69eccc2fcb62df0e1f
ShachakR <59149573+ShachakR@users.noreply.github.com>
2021-11-26 13:03:57 -0500

    mobile nav

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -9,21 +9,25 @@
         </div>
 
         <div>
-                   @if (Auth::guest())
-                   <div>
+            @if (Auth::guest())
+                <div>
                     <a class="nav-item-top" href="{{ route('login') }}">Log in</a>
                     <a class="nav-item-top" href="{{ route('register') }}">Register</a>
-                   </div>
-                   @else
-                   <div>
-                    <a class="nav-item-top" href="{{ route('profile', Auth::user()->username ) }}"> {{Auth::user()->username }}</a>
-                    <a class="nav-item-top" href="{{ route('logout') }}" onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();"><i class="material-icons-outlined nv-icon-28">logout</i>Logout </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                   </div>
-                   @endif
+                </div>
+                @else
+                <div class="dropdown">
+                    <button class="nav-item-top dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{Auth::user()->username }}
+                      </button>   
+                      <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();"><i class="material-icons-outlined nv-icon-24">logout</i>Logout </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </div>             
+                </div>
+            @endif
         </div>
 
     </div>
